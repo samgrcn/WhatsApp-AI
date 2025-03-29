42ed8fff00da1f338e8435e4594fc8373eb9c68b
Samuel Garcin <samgrcn@proton.me>
2025-03-29 19:00:41 +0100

    fixing build

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Install backend dependencies
+echo "Installing backend dependencies..."
+npm install
+
+# Install frontend dependencies
+echo "Installing frontend dependencies..."
+cd frontend
+npm install
+cd ..
+
 # Build frontend
 echo "Building frontend..."
 cd frontend
